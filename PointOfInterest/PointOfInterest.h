70a2eb5fb9ddb1141ed5859b571959a8cf5adcb1
André Roque Matheus <amatheus@mac.com>
2018-05-08 14:39:03 -0300

    Initial Commit

---
+++ PointOfInterest/PointOfInterest.h
@@ -0,0 +1,19 @@
+//
+//  PointOfInterest.h
+//  PointOfInterest
+//
+//  Created by André Roque Matheus on 08/05/2018.
+//  Copyright © 2018 André Roque Matheus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PointOfInterest.
+FOUNDATION_EXPORT double PointOfInterestVersionNumber;
+
+//! Project version string for PointOfInterest.
+FOUNDATION_EXPORT const unsigned char PointOfInterestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PointOfInterest/PublicHeader.h>
+
+
